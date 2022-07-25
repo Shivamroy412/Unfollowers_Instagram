57e795819efa6cd4ba38bec9107aeae7610ec6db
Shivam Roy <roy.shivam412@gmail.com>
2022-07-25 14:18:15 +0530

    Added config file to pass credentials

---
+++ config.py
@@ -0,0 +1,7 @@
+# You will have to disable Two-Factor Authetication in your Instagram if its activated.
+# Please follow the instructions given in the below link to turn two-factor authetication on/off
+# https://help.instagram.com/1124604297705184/?helpref=uf_share
+
+#Enter your Instagram username and password here as Strings
+USERNAME = ""
+PASSWORD = ""
